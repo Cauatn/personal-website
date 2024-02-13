3960eb0c58a33307101698bfb574c659a2d0de56
Cauã Tavares <tavarescauac@gmail.com>
2024-02-13 11:35:38 -0300

    feat: add CurriculumDialog component

---
+++ src/components/curriculum-pdf.tsx
@@ -0,0 +1,16 @@
+import { ArrowRightIcon } from "lucide-react";
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+
+export default function CurriculumDialog() {
+  return (
+    <Dialog>
+      <DialogTrigger className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-white px-8 text-sm font-medium shadow-sm gap-2 transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50  dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-950 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300">
+        Ver Curriculo
+        <ArrowRightIcon className="w-4 h-4" />
+      </DialogTrigger>
+      <DialogContent className="top-[50%] flex items-center justify-center p-8 pt-9">
+        <iframe src="src\components\teste.pdf" width="640" height="480" />
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,5 +1,6 @@
 import { ArrowRightIcon } from "@radix-ui/react-icons";
 import { Link } from "react-router-dom";
+import CurriculumDialog from "./curriculum-pdf";
 
 export default function Main() {
   function scrollToBottom() {
@@ -25,13 +26,7 @@ export default function Main() {
             </div>
 
             <div className="flex flex-col gap-2 min-[400px]:flex-row">
-              <Link
-                className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-white px-8 text-sm font-medium shadow-sm gap-2 transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50  dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-950 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
-                to={""}
-              >
-                Ver currículo
-                <ArrowRightIcon className="w-4 h-4" />
-              </Link>
+              <CurriculumDialog />
               <Link
                 to={""}
                 className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-white px-8 text-sm font-medium shadow-sm gap-2 transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50  dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-950 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
